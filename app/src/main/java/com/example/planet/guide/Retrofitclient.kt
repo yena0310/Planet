26a3167331c8c1a24d0abf7f38d6a66aa1a3f3ca
yena <124774586+yena0310@users.noreply.github.com>
2025-06-03 00:54:52 +0900

    분리배출 표시 IP 주소 변경, YOLO-INFO 숨김

--- app/src/main/java/com/example/planet/guide/Retrofitclient.kt
+++ app/src/main/java/com/example/planet/guide/Retrofitclient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://10.0.2.2:8000"
+    private const val BASE_URL = "http://172.19.29.122:8000"
 
     val apiService: RecyclingApiService by lazy {
         val retrofit = Retrofit.Builder()
